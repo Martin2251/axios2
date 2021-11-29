d470b419353856b66002e781a7cb3765edd0d4a7
martin lowe <martinlowe673@gmail.com>
2021-11-29 07:19:58 +0000

    basic code

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+const Index = () => {
+  const martin = {
+    name: "martin",
+    age: "31",
+    eyeColor: "hazel",
+    hobby: "music",
+    eyeGlasses: true,
+  };
+
+  martin.hobby;
+
+  return (
+    <div>
+      <h4>Object Review</h4>
+    </div>
+  );
+};
